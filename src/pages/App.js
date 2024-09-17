8a20bda2d6e09a6b78080c7f00f38ab101c7da8b
Natanael Melo <natanael.melo417@gmail.com>
2024-09-17 10:10:45 -0300

    desafio concluido

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,53 +1,49 @@
-
-import { useState } from 'react';
+import { useState } from 'react'
 import gitLogo from '../assets/github.png'
-import Input from '../components/Input';
-import Button from '../components/Button';
-import ItemRepo from '../components/ItemRepo';
-import { api } from '../services/api';
+import Input from '../components/Input'
+import Button from '../components/Button'
+import ItemRepo from '../components/ItemRepo'
+import { api } from '../services/api'
 
-import { Container } from './styles';
+import { Container } from './styles'
 
 function App() {
-
-  const [currentRepo, setCurrentRepo] = useState('');
-  const [repos, setRepos] = useState([]);
-
+  const [currentRepo, setCurrentRepo] = useState('')
+  const [repos, setRepos] = useState([])
 
   const handleSearchRepo = async () => {
+    const { data } = await api.get(`repos/${currentRepo}`)
 
-    const {data} = await api.get(`repos/${currentRepo}`)
+    if (data.id) {
+      const isExist = repos.find(repo => repo.id === data.id)
 
-    if(data.id){
-
-      const isExist = repos.find(repo => repo.id === data.id);
-
-      if(!isExist){
-        setRepos(prev => [...prev, data]);
+      if (!isExist) {
+        setRepos(prev => [...prev, data])
         setCurrentRepo('')
         return
       }
-
     }
     alert('Repositório não encontrado')
-
   }
 
-  const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
+  const handleRemoveRepo = id => {
+    const updatedRepos = repos.filter(repo => repo.id !== id)
+    setRepos(updatedRepos)
   }
-
 
   return (
     <Container>
-      <img src={gitLogo} width={72} height={72} alt="github logo"/>
-      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
-      <Button onClick={handleSearchRepo}/>
-      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+      <img src={gitLogo} width={72} height={72} alt="github logo" />
+      <Input
+        value={currentRepo}
+        onChange={e => setCurrentRepo(e.target.value)}
+      />
+      <Button onClick={handleSearchRepo} />
+      {repos.map(repo => (
+        <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo} />
+      ))}
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
